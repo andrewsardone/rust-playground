88ad92d9b9ba43b39e6db620b494345876dd31e7
Andrew Sardone <andrew@andrewsardone.com>
2014-04-18 07:25:32 -0400

    Link to `assert` macro in testing notes

--- rust_for_rubyists/testing.rs
+++ rust_for_rubyists/testing.rs
@@ -6,7 +6,21 @@
 //
 //     rustc --test testing.rs
 
+```rust
 #[test]
 fn this_tests_code() {
     fail!("Fail!");
 }
+```
+
+There's also a traditional [`assert` macro][stdma] that calls `fail` if the
+given condition evaluates to false.
+
+```rust
+#[test]
+fn this_tests_equality() {
+    assert!(1 == 1);
+}
+```
+
+[stdma]: http://static.rust-lang.org/doc/master/std/macros/macro.assert.html
